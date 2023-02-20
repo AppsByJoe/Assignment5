cc42840094d2a3bb59c48e51239a80da77660149
AppsByJoe <jth0124@auburn.edu>
2023-02-19 19:50:15 -0500

    segue with warning (?)

---
+++ Assignment5/View/ConversionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConversionViewController.swift
+//  Assignment5
+//
+//  Created by Hronek Joseph on 2/19/23.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
