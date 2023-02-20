8da3417714dbf9518935c9e4af54fb1f8cb4e71c
AppsByJoe <jth0124@auburn.edu>
2023-02-19 19:40:01 -0500

    Added MVC structure, created loanlogic file and viewcontroller switch state variables.

---
+++ Assignment5/Model/ConverterLogic.swift
@@ -0,0 +1,8 @@
+//
+//  ConverterLogic.swift
+//  Assignment5
+//
+//  Created by Hronek Joseph on 2/19/23.
+//
+
+import Foundation
---
+++ Assignment5/View/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Assignment5
+//
+//  Created by Hronek Joseph on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // var converterLogic = ConverterLogic()
+    var poundIsVisible = true
+    var euroIsVisible = true
+    var yenIsVisible = true
+    var pesoIsVisible = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toLoanAmount" {
+            let converterNavigation = segue.destination as! ConversionViewController
+            converterNavigation.poundIsVisible = poundIsVisible
+            converterNavigation.euroIsVisible = euroIsVisible
+            converterNavigation.yenIsVisible = yenIsVisible
+            converterNavigation.pesoIsVisible = pesoIsVisible
+        }
+    }
+    */
+}
+
